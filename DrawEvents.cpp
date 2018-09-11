785601b064a41170cb74822f49476066e3415fad
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-11 19:26:30 +0200

    Compiles and links locally

--- DrawEvents.cpp
+++ DrawEvents.cpp
@@ -5,13 +5,13 @@
 #include "DrawEvents.h"
 
 // --- distribution functions ---
-inline int drawDisEvent(const std::vector<double> &vecRates, std::mt19937_64 &prng) {
+int drawDisEvent(const std::vector<double> &vecRates, std::mt19937_64 &prng) {
 //draw a discrete distribution event
     std::discrete_distribution<int> drawEvent(vecRates.begin(), vecRates.end());
     return drawEvent(prng);
 }
 
-inline int drawUniEvent(const int &botBoundary, const int &topBoundary, std::mt19937_64 &prng) {
+int drawUniEvent(const int &botBoundary, const int &topBoundary, std::mt19937_64 &prng) {
 //draw a uniform distribution event
     // using "topBoundary" (= size of vector - 1 = position of last element of vector)
     // because this function works with different types of vectors: vector<int> & vector<pair<int,int> >
